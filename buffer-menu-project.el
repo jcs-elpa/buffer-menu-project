27b77c98f9178e3bd2657204a7b4fa03d321b2a9
Jen-Chieh <jcs090218@gmail.com>
2022-02-17 15:27:40 +0800

    Add src

---
+++ buffer-menu-project.el
@@ -0,0 +1,38 @@
+;;; buffer-menu-project.el --- List buffers relative to project  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-02-17 15:26:10
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: List buffers relative to project.
+;; Keyword: buffer menu project
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs-elpa/buffer-menu-project
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; List buffers relative to project.
+;;
+
+;;; Code:
+
+
+
+(provide 'buffer-menu-project)
+;;; buffer-menu-project.el ends here
